a514454b347593abed3744513807a64f630832c6
Daniel Goldsmith <dgold@ascraeus.org>
2017-06-14 20:00:21 +0100

    final vers of icomoon

--- tests.php
+++ tests.php
@@ -5,111 +5,53 @@
 
 $json = '
 {
-  "meta": {
-    "code": 200,
-    "server": "31.208"
-  },
-  "data": [
-    {
-      "id": 151880,
-      "parent_id": false,
-      "title": "",
-      "slug": "151880",
-      "type": "post.social",
-      "privacy": "visibility.public",
-      "guid": "e76d0",
-      "content": {
-        "text": "Good morning campers! Rise and Shine!",
-        "html": "<p>Good morning campers! Rise and Shine!</p>",
-        "summary": false,
-        "banner": false,
-        "is_edited": false
-      },
-      "audio": false,
-      "tags": false,
-      "geo": "",
-      "files": false,
-      "urls": {
-        "canonical_url": "/post/151880",
-        "full_url": "10centuries.org/post/151880",
-        "alt_url": "10centuries.org/151880",
-        "is_https": true
-      },
-      "thread": false,
-      "mentions": false,
-      "account": [
-        {
-          "id": 124,
-          "avatar_url": "//cdn.10centuries.org/j3J28w/b0c497ec9071170ec3a5483581c9df49.jpg",
-          "username": "dgold",
-          "canonical_url": "https://10centuries.org/profile/dgold",
-          "podcast_rss": false,
-          "name": {
-            "first_name": "Daniel",
-            "last_name": "Goldsmith",
-            "display": "dgold"
-          },
-          "counts": {
-            "following": 11,
-            "followers": 14,
-            "stars": 39,
-            "socialposts": 172,
-            "blogposts": 0,
-            "podcasts": 0
-          },
-          "description": {
-            "text": "",
-            "html": ""
-          },
-          "created_at": "2017-01-16T09:29:48Z",
-          "timezone": "UTC",
-          "verified": {
-            "is_verified": false,
-            "url": ""
-          },
-          "annotations": false,
-          "cover_image": false,
-          "evangelist": false,
-          "follows_you": true,
-          "you_follow": true,
-          "is_muted": false,
-          "is_silenced": false
-        }
-      ],
-      "channel": {
-        "id": 1,
-        "owner_id": false,
-        "type": "channel.global",
-        "privacy": "visibility.public",
-        "guid": "d9ba5",
-        "created_at": "2015-08-01T00:00:00Z",
-        "created_unix": 1438387200,
-        "updated_at": "2015-08-01T00:00:00Z",
-        "updated_unix": 1438387200
-      },
-      "client": {
-        "hash": "60a52",
-        "name": "Known Integration"
-      },
-      "created_at": "2017-06-09T09:58:17Z",
-      "created_unix": 1497002297,
-      "publish_at": "2017-06-09T09:58:17Z",
-      "publish_unix": 1497002297,
-      "updated_at": "2017-06-09T09:58:17Z",
-      "updated_unix": 1497002297,
-      "expires_at": false,
-      "expires_unix": false,
-      "is_visible": true,
-      "is_muted": false,
-      "is_deleted": false
-    }
-  ]
+    "id": 9134498,
+    "created_at": "2017-06-14T10:12:04.591Z",
+    "in_reply_to_id": null,
+    "in_reply_to_account_id": null,
+    "sensitive": null,
+    "spoiler_text": "",
+    "visibility": "public",
+    "language": "en",
+    "application": {
+        "name": "caconym",
+        "website": null
+    },
+    "account": {
+        "id": 44471,
+        "username": "dgold",
+        "acct": "dgold",
+        "display_name": "Daniel Goldsmith 🍷",
+        "locked": false,
+        "created_at": "2017-04-04T11:31:19.151Z",
+        "followers_count": 4,
+        "following_count": 12,
+        "statuses_count": 14,
+        "note": "<p>Sometimes I feel like I&apos;m from another planet</p>",
+        "url": "https://mastodon.social/@dgold",
+        "avatar": "https://files.mastodon.social/accounts/avatars/000/044/471/original/a90b11184a0beaba.png",
+        "avatar_static": "https://files.mastodon.social/accounts/avatars/000/044/471/original/a90b11184a0beaba.png",
+        "header": "/headers/original/missing.png",
+        "header_static": "/headers/original/missing.png"
+    },
+    "media_attachments": [],
+    "mentions": [],
+    "tags": [],
+    "uri": "tag:mastodon.social,2017-06-14:objectId=9134498:objectType=Status",
+    "content": "<p>This is a short test post. Saluton mondo!</p>",
+    "url": "https://mastodon.social/@dgold/9134498",
+    "reblogs_count": 0,
+    "favourites_count": 0,
+    "reblog": null,
+    "favourited": false,
+    "reblogged": false,
+    "muted": false
 }';
 
 $arrayName = json_decode($json, true);
 //echo $arrayName['meta']['server'];
-$tenclink = "https://" . $arrayName['data']['0']['urls']['full_url'];
-echo $tenclink
+$link = $arrayName['url'];
+echo $link
 //		'userName'
 //	'siteUrl'
 //	'siteTitle'
