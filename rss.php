fc949ea0b8011794e8a6931194cb85fa1e43a6a7
Daniel Goldsmith <dgold@ascraeus.org>
2017-06-16 16:35:44 +0100

    bugfix for rss cotinued

--- rss.php
+++ rss.php
@@ -26,11 +26,11 @@
 	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 	$configs = include('configs.php');
-	header("Content-Type: text/xml;charset=iso-8859-1");
+	header("Content-Type: text/xml;charset=utf-8");
 	// include 'wp_comments.php';
 	$results = file_get_contents('text.txt');
 	// echo $results;
-	echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n";
+	echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n";
 ?>
 <rss version="2.0">
 	<channel>
